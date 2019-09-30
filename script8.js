0b3d2ba278e2753e5c7ff33f9e38da33ffe5b156
Liviu Preda <liviupreda@gmail.com>
2019-09-30 09:24:29 +0300

    async / await

--- script8.js
+++ script8.js
@@ -43,6 +43,59 @@
 // getRecipe();
 
 // -- PROMISES
+// const getIDs = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve([523, 883, 432, 974]);
+//     // no need to call the reject function here. setTimeout always finishes
+//   }, 1500);
+// });
+
+// // function that receives ID and returns a promise
+// const getRecipe = recID => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       id => {
+//         const recipe = { title: 'Pasta Bolognese', publisher: 'Liv' };
+//         resolve(`${id}: ${recipe.title} by ${recipe.publisher}`);
+//       },
+//       1500,
+//       recID
+//     );
+//   });
+// };
+
+// const getRelated = publisher => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       pub => {
+//         const recipe = { title: 'Pizza al Cippola', publisher: 'Liv' };
+//         resolve(`${pub}: ${recipe.title}`);
+//       },
+//       1500,
+//       publisher
+//     );
+//   });
+// };
+
+// // Consume promises
+// getIDs
+//   .then(IDs => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]); // returns a promise
+//   })
+//   .then(recipe => {
+//     // Chaining
+//     console.log(recipe);
+//     return getRelated('Liviu Preda');
+//   })
+//   .then(recipe => {
+//     console.log(recipe);
+//   })
+//   .catch(error => {
+//     console.log('Something went wrong');
+//   });
+
+// -- ASYNC/ AWAIT ES8 - designed to CONSUME PROMISES, not to produce them
 const getIDs = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve([523, 883, 432, 974]);
@@ -77,20 +130,23 @@ const getRelated = publisher => {
   });
 };
 
-// Consume promises
-getIDs
-  .then(IDs => {
-    console.log(IDs);
-    return getRecipe(IDs[2]); // returns a promise
-  })
-  .then(recipe => {
-    // Chaining
-    console.log(recipe);
-    return getRelated('Liviu Preda');
-  })
-  .then(recipe => {
-    console.log(recipe);
-  })
-  .catch(error => {
-    console.log('Something went wrong');
-  });
+// Asynchronous function; runs in the background and is non- blocking
+// returns a promise; Await can only be used inside an async function,
+// because it stops the code in that function from executing
+// until the promise is resolved
+async function getRecipesAW() {
+  // consume the three promises
+  const IDs = await getIDs; // stops the code from executing until promise is resolved
+  console.log(IDs);
+  const recipe = await getRecipe(IDs[2]);
+  console.log(recipe);
+  const related = await getRelated('Liviu Preda');
+  console.log(related);
+
+  // If we return a value from an async function, the
+  // returned promise from the function will be resolved with that same value
+  return recipe;
+}
+getRecipesAW().then(promiseResult =>
+  console.log(`${promiseResult} async return test`)
+);
